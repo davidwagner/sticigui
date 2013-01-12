fb0f20d186790bf7f9f58131ebbc5d0051529df8
Philip B. Stark <stark@risklaw.org>
2013-01-12 01:38:27 -0800

    populating

---
+++ SticiGui/Print/pdfAll.pl
@@ -0,0 +1,11 @@
+#usr/local/bin/perl
+    opendir (DIR, ".");
+    fileloop: while ( $d = readdir(DIR)) {
+        print "file $d\n";
+        if ( $d =~ m/\.ps/ ) {
+            system "ps2pdf -dEmbedAllFonts $d";
+        }
+
+    }
+
+
