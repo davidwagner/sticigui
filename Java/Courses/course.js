8b34d8cf62ad9df503ed881a54bddd9734f81641
Philip B. Stark <stark@risklaw.org>
2013-01-12 11:01:20 -0800

    incremental changes to hw authentication

--- Java/Courses/course.js
+++ Java/Courses/course.js
@@ -19,11 +19,11 @@ var courseName;                    // English name of course
 var scoreBase = "../../../cgi-bin/scores.dat?";
 var queryBase = "http://statistics.berkeley.edu/cgi-bin/querygrade5?";
 var courses = [
-               ["0","","none","","","",1],
-               ["1","s21-su12","Statistics W21, Summer 2012, UC Berkeley","stark","Prof. P.B. Stark",
-                      "Teach/S21/Su12/Grades", 5, 4],
+               ["0","s2-x","Statistics 2.1x, Spring 2013, UC Berkeley","stark","Profs P.B. Stark and A. Adhikari","",5,4],
+               ["1","s21-s13","Statistics W21, Spring 2013, UC Berkeley","stark","Prof. S. Murali",
+                      "Teach/S21/Su13/Grades", 5, 4],
                ["2","guest-","guest login (no grading)","stark","Prof. P.B. Stark","","",1],
-               ["3","s21-f09","Statistics 21, Fall 2009, UC Berkeley","stark","Prof. P.B. Stark","","",1]
+               ["3","","none","","","",1]
               ];
 var dFileBase = "sets.due";        // due date file stem
 var sFileBase = "sid.txt";         // user file stem
